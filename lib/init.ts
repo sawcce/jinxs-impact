e5f2ed6a3e65f83a2bf650acc0dee204cb62c590
sawcce <alexis.collieux@gmail.com>
2022-02-22 20:05:00 +0100

    Automatically creating build/routes folder on init

--- lib/init.ts
+++ lib/init.ts
@@ -1,72 +1,76 @@
-import { resolve, join } from "@/path.ts";
-import { Confirm, Input, prompt } from "@/cliffy.ts";
-import { ensureDir } from "@/fs.ts";
+import { resolve, join } from '@/path.ts';
+import { Confirm, Input, prompt } from '@/cliffy.ts';
+import { ensureDir } from '@/fs.ts';
 
 export default async function Init(this: any) {
-  let name = "";
+  let name = '';
 
   const config = await prompt([
     {
       type: Input,
-      name: "projectName",
-      message: "Choose a name for the project",
-      default: "sample-app",
+      name: 'projectName',
+      message: 'Choose a name for the project',
+      default: 'sample-app',
       after: async function ({ projectName }, next) {
-        name = projectName || "";
+        name = projectName || '';
         await next();
-      },
+      }
     },
     {
       type: Input,
-      name: "projectDir",
-      message: "Choose a root directory (empty for cwd)",
+      name: 'projectDir',
+      message: 'Choose a root directory (empty for cwd)',
       default: `/${name}`,
-      suggestions: [".", "./" + name],
-      hint: "$name = project name ; . for current directory",
+      suggestions: ['.', './' + name],
+      hint: '$name = project name ; . for current directory',
       before: async function (this: { default: string }, _, next) {
         this.default = `./${name}`;
         await next();
       },
       transform: (value: string) => {
-        return value.replaceAll("$name", name);
-      },
+        return value.replaceAll('$name', name);
+      }
     },
     {
       type: Input,
-      name: "routesDir",
-      message: "Choose a routes directory",
-      default: "routes",
-      suggestions: ["routes", "pages", "endpoints"],
+      name: 'routesDir',
+      message: 'Choose a routes directory',
+      default: 'routes',
+      suggestions: ['routes', 'pages', 'endpoints']
     },
     {
       type: Input,
-      name: "outputDir",
-      message: "Choose a build directory",
-      default: "build",
-      suggestions: ["build", "public"],
+      name: 'outputDir',
+      message: 'Choose a build directory',
+      default: 'build',
+      suggestions: ['build', 'public']
     },
     {
       type: Confirm,
-      name: "ok",
-      message: "Does this look good to you ?",
+      name: 'ok',
+      message: 'Does this look good to you ?',
       after: async ({ ok }, next) => {
         if (ok) {
           await next();
         } else {
-          await next("projectName");
+          await next('projectName');
         }
-      },
-    },
+      }
+    }
   ]);
 
-  const folder = resolve(Deno.cwd(), config.projectDir || "");
+  const folder = resolve(Deno.cwd(), config.projectDir || '');
+
   await ensureDir(folder);
+
+  await ensureDir(join(folder, '/', config.routesDir || 'routes'));
+  await ensureDir(join(folder, '/', config.outputDir || 'build'));
 
   delete config.ok;
   delete config.projectDir;
 
   await Deno.writeTextFile(
-    join(folder, "/impact.config.ts"),
-    `export default ${JSON.stringify(config, null, "\t")}`
+    join(folder, '/impact.config.ts'),
+    `export default ${JSON.stringify(config, null, '\t')}`
   );
 }
