b101737a214c5c40f536a90d70606f20e384588d
sawcce <alexis.collieux@gmail.com>
2022-02-24 18:28:05 +0100

    Partially working dev server

--- lib/dev.ts
+++ lib/dev.ts
@@ -3,7 +3,10 @@ import { serve } from '@/http.ts';
 import { METHOD } from '$/net.ts';
 import { Endpoint } from '$/routing.ts';
 
-const Endpoints: Record<METHOD, Endpoint[]> = {
+type ContentType = 'text/html' | 'application/json';
+type FunctionByUrl = [RegExp, Function, ContentType];
+
+const Endpoints: Record<METHOD, FunctionByUrl[]> = {
   GET: [],
   HEAD: [],
   POST: [],
@@ -15,31 +18,56 @@ const Endpoints: Record<METHOD, Endpoint[]> = {
   PATCH: []
 };
 
+function pushRoute(
+  method: METHOD,
+  regex: RegExp,
+  handler: Function,
+  type: ContentType = 'application/json'
+) {
+  Endpoints[method].push([regex, handler, type]);
+}
+
 export async function Server(file: string) {
   const module = await import(file);
-  const endpoints = module.routes;
+  const endpoints: any[] = module.routes;
 
   for (const endpoint of endpoints) {
     for (const method of Object.keys(endpoint.methods)) {
-      Endpoints[method as METHOD].push(endpoint);
+      const regex = endpoint.regex;
+
+      if (method == 'GET') {
+        let handler = (params: any) => endpoint.module.get(params);
+
+        if (endpoint.hasDefault == true) {
+          handler = (params: any) =>
+            endpoint.layout(endpoint.module.default(params)).emit();
+
+          pushRoute(method, regex, handler, 'text/html');
+          continue;
+        }
+
+        pushRoute(method, regex, handler);
+      }
     }
   }
 
-  console.log(Endpoints);
   await serve(handler, { port: 8080 });
 }
 
 const handler = (request: Request): Response => {
-  console.log(request.method);
-
   for (const endpoint of Endpoints[request.method as METHOD]) {
-    if (request.url.match(endpoint.regex)?.length != 0) {
-      console.log('MATCH!');
+    const pathname = new URL(request.url).pathname;
+    const match = pathname.match(endpoint[0]);
+
+    if (match != null && match.length != 0) {
+      const headers = new Headers();
+      headers.set('Content-Type', endpoint[2]);
+
+      return new Response(endpoint[1](), { status: 200, headers });
     }
   }
 
-  let body = 'Your user-agent is:\n\n';
-  body += request.headers.get('user-agent') || 'Unknown';
+  const body = '404 not found';
 
-  return new Response(body, { status: 200 });
+  return new Response(body, { status: 404 });
 };
